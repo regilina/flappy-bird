fed2efeed2f58474883a3d65c9e5bac820153e0b
Regi <gt12139@gmail.com>
2023-08-24 21:24:33 +0300

    The background is moving.

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
-const imgURL = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
+const imgURL = 'https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png'
 
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
 
-const img = new Image();
-img.src = imgURL;
+const img = new Image()
+img.src = imgURL
 
-const SPEED = 3.1;
-let index = 0;
+const SPEED = 3.1
+let index = 0
 
 const render = () => {
-  index += 0.3;
-  
-  const backgroudX = -((index * SPEED) % canvas.width);
+  index += 0.3
+
+  const backgroudX = -((index * SPEED) % canvas.width)
 
   const bgSource = {
     x: 0,
     y: 0,
     width: canvas.width,
-    height: canvas.height,
-  };
+    height: canvas.height
+  }
 
   const bgPartOneResult = {
     x: backgroudX + canvas.width,
     y: 0,
     width: canvas.width,
-    height: canvas.height,
-  };
-
+    height: canvas.height
+  }
 
   // вторая часть фонового изображения, которая
   // идёт следом за первой
@@ -35,8 +34,8 @@ const render = () => {
     x: backgroudX,
     y: 0,
     width: canvas.width,
-    height: canvas.height,
-  };
+    height: canvas.height
+  }
 
   ctx.drawImage(
     img,
@@ -50,8 +49,8 @@ const render = () => {
     bgPartOneResult.y,
     bgPartOneResult.width,
     bgPartOneResult.height
-  );
-  
+  )
+
   ctx.drawImage(
     img,
 
@@ -64,9 +63,9 @@ const render = () => {
     bgPartTwoResult.y,
     bgPartTwoResult.width,
     bgPartTwoResult.height
-  );
+  )
 
-  window.requestAnimationFrame(render);
-};
+  window.requestAnimationFrame(render)
+}
 
-img.onload = render;
+img.onload = render
