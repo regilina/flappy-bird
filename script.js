51a573fd2596ea3b0fa7ca370a9b721f69910570
Regi <gt12139@gmail.com>
2023-09-07 00:19:39 +0300

    Classes Game, Bird, Background created.

--- script.js
+++ script.js
@@ -1,105 +1,120 @@
-const imgURL = 'https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png'
-
-const canvas = document.getElementById('canvas')
-const ctx = canvas.getContext('2d')
-
-const img = new Image()
-img.src = imgURL
-
-const SPEED = 3.1
-
-// ширина и высота птицы
-const SIZE = [51, 36]
-
-let index = 0
-
-const render = () => {
-  index += 0.3
-
-  const backgroudX = -((index * SPEED) % canvas.width)
-
-  const bgSource = {
-    x: 0,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
+class Background {
+  constructor (img, canvas) {
+    this.img = img
+    this.canvas = canvas
+    this.ctx = canvas.getContext('2d')
+    this.SPEED = 3.1
+    this.index = 0
   }
 
-  const bgPartOneResult = {
-    x: backgroudX + canvas.width,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
+  render () {
+    this.index += 0.3
+    const backgroudX = -((this.index * this.SPEED) % this.canvas.width)
+
+    const bgSource = {
+      x: 0,
+      y: 0,
+      width: this.canvas.width,
+      height: this.canvas.height
+    }
+
+    const bgPartOneResult = {
+      x: backgroudX + this.canvas.width,
+      y: 0,
+      width: this.canvas.width,
+      height: this.canvas.height
+    }
+
+    const bgPartTwoResult = {
+      x: backgroudX,
+      y: 0,
+      width: this.canvas.width,
+      height: this.canvas.height
+    }
+
+    this.ctx.drawImage(
+      this.img,
+      bgSource.x,
+      bgSource.y,
+      bgSource.width,
+      bgSource.height,
+      bgPartOneResult.x,
+      bgPartOneResult.y,
+      bgPartOneResult.width,
+      bgPartOneResult.height
+    )
+
+    this.ctx.drawImage(
+      this.img,
+      bgSource.x,
+      bgSource.y,
+      bgSource.width,
+      bgSource.height,
+      bgPartTwoResult.x,
+      bgPartTwoResult.y,
+      bgPartTwoResult.width,
+      bgPartTwoResult.height
+    )
   }
-
-  const bgPartTwoResult = {
-    x: backgroudX,
-    y: 0,
-    width: canvas.width,
-    height: canvas.height
-  }
-
-  ctx.drawImage(
-    img,
-
-    bgSource.x,
-    bgSource.y,
-    bgSource.width,
-    bgSource.height,
-
-    bgPartOneResult.x,
-    bgPartOneResult.y,
-    bgPartOneResult.width,
-    bgPartOneResult.height
-  )
-
-  ctx.drawImage(
-    img,
-
-    bgSource.x,
-    bgSource.y,
-    bgSource.width,
-    bgSource.height,
-
-    bgPartTwoResult.x,
-    bgPartTwoResult.y,
-    bgPartTwoResult.width,
-    bgPartTwoResult.height
-  )
-
-  // изображение птицы, которое копируем
-  // из изображения-источника
-  const birdSource = {
-    x: 432,
-    y: Math.floor((index % 9) / 3) * SIZE[1],
-    width: SIZE[0],
-    height: SIZE[1]
-  }
-
-  // координаты, по которым птица
-  // будет расположена на Canvas
-  const birdResult = {
-    x: canvas.width / 2 - SIZE[0] / 2,
-    y: 200,
-    width: SIZE[0],
-    height: SIZE[1]
-  }
-
-  ctx.drawImage(
-    img,
-
-    birdSource.x,
-    birdSource.y,
-    birdSource.width,
-    birdSource.height,
-
-    birdResult.x,
-    birdResult.y,
-    birdResult.width,
-    birdResult.height
-  )
-
-  window.requestAnimationFrame(render)
 }
 
-img.onload = render
+class Bird {
+  constructor (img, canvas) {
+    this.img = img
+    this.canvas = canvas
+    this.ctx = canvas.getContext('2d')
+    this.SIZE = [51, 36]
+    this.index = 0
+  }
+
+  render () {
+    const birdSource = {
+      x: 432,
+      y: Math.floor((this.index % 9) / 3) * this.SIZE[1],
+      width: this.SIZE[0],
+      height: this.SIZE[1]
+    }
+
+    const birdResult = {
+      x: this.canvas.width / 2 - this.SIZE[0] / 2,
+      y: 200,
+      width: this.SIZE[0],
+      height: this.SIZE[1]
+    }
+
+    this.ctx.drawImage(
+      this.img,
+      birdSource.x,
+      birdSource.y,
+      birdSource.width,
+      birdSource.height,
+      birdResult.x,
+      birdResult.y,
+      birdResult.width,
+      birdResult.height
+    )
+  }
+}
+
+class Game {
+  constructor (imgURL, canvasId) {
+    this.img = new Image()
+    this.img.src = imgURL
+    this.canvas = document.getElementById(canvasId)
+    this.background = new Background(this.img, this.canvas)
+    this.bird = new Bird(this.img, this.canvas)
+
+    this.img.onload = () => this.render()
+  }
+
+  render () {
+    this.background.render()
+    this.bird.render()
+    window.requestAnimationFrame(() => this.render())
+  }
+}
+
+const imgURL = 'https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png'
+const canvasId = 'canvas'
+
+const game = new Game(imgURL, canvasId)
